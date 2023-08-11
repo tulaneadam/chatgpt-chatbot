54c268e2029ab20f686c14dec56b8cbd4f6c0fdc
Adam Goldfine <tulaneadam21@gmail.com>
2023-08-11 18:22:06 -0400

    .

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import openai from './config/open-ai.js';
 import readlineSync from 'readline-sync';
 import colors from 'colors';
 
+const MAX_RETRIES = 3; // Number of retries
+const RETRY_DELAY = 5000; // Delay in milliseconds
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function main() {
   console.log(colors.bold.green('Welcome to the Chatbot Program!'));
   console.log(colors.bold.green('You can start chatting with the bot.'));
@@ -11,37 +18,49 @@ async function main() {
   while (true) {
     const userInput = readlineSync.question(colors.yellow('You: '));
 
-    try {
-      // Construct messages by iterating over the history
-      const messages = chatHistory.map(([role, content]) => ({
-        role,
-        content,
-      }));
+    // Retry loop
+    for (let retries = 0; retries < MAX_RETRIES; retries++) {
+      try {
+        // Construct messages by iterating over the history
+        const messages = chatHistory.map(([role, content]) => ({
+          role,
+          content,
+        }));
 
-      // Add latest user input
-      messages.push({ role: 'user', content: userInput });
+        // Add latest user input
+        messages.push({ role: 'user', content: userInput });
 
-      // Call the API with user input & history
-      const completion = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: messages,
-      });
+        // Call the API with user input & history
+        const completion = await openai.createChatCompletion({
+          model: 'gpt-3.5-turbo',
+          messages: messages,
+        });
 
-      // Get completion text/content
-      const completionText = completion.data.choices[0].message.content;
+        // Get completion text/content
+        const completionText = completion.data.choices[0].message.content;
 
-      if (userInput.toLowerCase() === 'exit') {
+        if (userInput.toLowerCase() === 'exit') {
+          console.log(colors.green('Bot: ') + completionText);
+          return;
+        }
+
         console.log(colors.green('Bot: ') + completionText);
-        return;
+
+        // Update history with user input and assistant response
+        chatHistory.push(['user', userInput]);
+        chatHistory.push(['assistant', completionText]);
+
+        // Break out of the retry loop if successful
+        break;
+      } catch (error) {
+        if (error.response && error.response.status === 429) {
+          console.error(colors.red('Rate limit exceeded, retrying in 5 seconds...'));
+          await sleep(RETRY_DELAY); // Wait before retrying
+        } else {
+          console.error(colors.red(error));
+          break; // Exit retry loop for other errors
+        }
       }
-
-      console.log(colors.green('Bot: ') + completionText);
-
-      // Update history with user input and assistant response
-      chatHistory.push(['user', userInput]);
-      chatHistory.push(['assistant', completionText]);
-    } catch (error) {
-      console.error(colors.red(error));
     }
   }
 }
